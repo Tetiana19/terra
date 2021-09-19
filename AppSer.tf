9412cca9ec187d20dae8450adc7f48a24e7cccea
Tetiana Dinkievych <80890516+Tetiana19@users.noreply.github.com>
2021-09-19 16:42:05 +0300

    Update AppSer.tf

--- AppSer.tf
+++ AppSer.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.77.0"
-    }
+  required_version = "> 0.12.0"
+  
+  backend "azurerm" {
   }
 }
 
 provider "azurerm" {
+  version = ">=2.0.0"
   features {}
 }
 
