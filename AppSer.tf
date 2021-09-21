841e870b687154981e39509b6b66cb622bba54c2
Tetiana Dinkievych <80890516+Tetiana19@users.noreply.github.com>
2021-09-21 14:36:57 +0300

    Update AppSer.tf

--- AppSer.tf
+++ AppSer.tf
@@ -5,9 +5,6 @@
 #  version = ">=2.0.79"
 #  features {}
 #}
-variable "client_secret" {
-  client_secret   = "VITN4WwZMLOwBWjaJ-kRaJNrupWEDXC3BZ"
-}
 terraform {
   required_providers {
     azurerm = {
@@ -23,7 +20,7 @@ provider "azurerm" {
 
  subscription_id = "16acbe7c-85aa-4236-af7b-3583b1869ee7"
   client_id       = "6e4b4e8e-c7dc-40ab-a7c0-0e823507f5cd"
-  client_secret   = var.client_secret
+  client_secret   = "VITN4WwZMLOwBWjaJ-kRaJNrupWEDXC3BZ"
   tenant_id       = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
 }
 
