72f2a3aa0e42526ed397896beff4aaf3453b5824
Tetiana Dinkievych <80890516+Tetiana19@users.noreply.github.com>
2021-09-22 21:07:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -323,4 +323,29 @@ resource "azurerm_linux_virtual_machine" "devenv" {
 
 
 
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.prodenv.id
+}
+
+output "vm_ip" {
+  value = azurerm_linux_virtual_machine.prodenv.public_ip_address
+}
+
+output "tls_private_key" { 
+  value = tls_private_key.prodenv.private_key_pem 
+}
+
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.devenv.id
+}
+
+output "vm_ip" {
+  value = azurerm_linux_virtual_machine.devenv.public_ip_address
+}
+
+output "tls_private_key" { 
+  value = tls_private_key.devenv.private_key_pem 
+}
+
+
 
